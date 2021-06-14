be2aab5b4efd5c2501e569e08ecf54a8a5fea98a
Tyrone Hunt <tyrone.hunt07@gmail.com>
2021-06-14 20:08:23 +0100

    allowed user input to create modifiable request to weather App

--- Web Development/WeatherProject express/app.js
+++ Web Development/WeatherProject express/app.js
@@ -1,14 +1,27 @@
 const express=require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-// The GET request from the end-users browser to our server at / will in turn create a GET request to openweathermap
+app.use(bodyParser.urlencoded({extended: true}));
+
+// The GET request from the end-user's browser to our server at home '/'
+// will send over the index.html file
 app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html")
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=a651691c6e7a3c4f3df9582c6dad929f&units=metric"
+})
 
-  // GET request to openweathermap
+app.post("/", function(req, res) {
+
+  // Catch the data submitted via the form post in index.html
+  const query = req.body.cityName;
+  const apiKey = "a651691c6e7a3c4f3df9582c6dad929f"
+  const units = "metric"
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query +"&appid=" + apiKey + "&units=" + units
+
+  // Send GET request to openweathermap with the form values
   https.get(url, function(response){
     console.log(response.statusCode);
 
@@ -22,12 +35,13 @@ app.get("/", function(req, res){
       const icon = weatherData.weather[0].icon;
       const imgUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
       res.write("<p>" + weatherDescription +  "<p>")
-      res.write("<h1>The temperature in London is " + temp + "C</h1>")
+      res.write("<h1>The temperature in " +  query + " is " + temp + "C</h1>")
       res.write("<img src=" + imgUrl + ">")
       res.send()
       console.log(weatherDescription);
     })
   })
+
 })
 
 
