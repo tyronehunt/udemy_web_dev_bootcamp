3e8c9590cebc7a76f33aee2bd9cf06ec844056c4
Tyrone Hunt <tyrone.hunt07@gmail.com>
2021-06-15 17:53:24 +0100

    working mailchimp API for posting sign ups

--- Web Development/Mailchimp/app.js
+++ Web Development/Mailchimp/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 
@@ -19,11 +20,41 @@ app.get("/", function(req, res) {
 
 // Post data from form
 app.post("/", function(req, res){
-  var firstName = req.body.fName;
-  var lastName = req.body.lName;
-  var email = req.body.email;
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
 
-  console.log(firstName, lastName, email)
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+  const list_id = "c7b586589d";
+  const test_api_key = "cf6455253a9ac119881baba69ddaef4e-us6";
+  const url = "https:/us6.api.mailchimp.com/3.0/lists/" + list_id;
+  const options = {
+    method: "POST",
+    auth: "forgiggles:" + test_api_key
+  };
+
+  const request = https.request(url, options, function(response){
+    response.on("data", function(data){
+      console.log(JSON.parse(data));
+    })
+  })
+
+  request.write(jsonData);
+  request.end();
+
 });
 
 
