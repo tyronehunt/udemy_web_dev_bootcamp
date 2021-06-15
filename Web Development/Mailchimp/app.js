1e9db2b59e9dabc8df4ca906b5a98ad7563bf710
Tyrone Hunt <tyrone.hunt07@gmail.com>
2021-06-15 18:37:18 +0100

    working Mailchimp API with success and failure landing pages

--- Web Development/Mailchimp/app.js
+++ Web Development/Mailchimp/app.js
@@ -47,15 +47,30 @@ app.post("/", function(req, res){
   };
 
   const request = https.request(url, options, function(response){
+
+    // check for response errors
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+    // Log the response from Mailchimp
     response.on("data", function(data){
       console.log(JSON.parse(data));
-    })
-  })
+    });
+  });
 
   request.write(jsonData);
   request.end();
 
 });
+
+
+// POST request from failure route
+app.post("/failure", function(req, res){
+  res.redirect("/")
+})
 
 
 app.listen(3000, function(){
