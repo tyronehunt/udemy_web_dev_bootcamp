80eb5fd730c571ba7f6efc494ce533da061d5139
Tyrone Hunt <tyrone.hunt07@gmail.com>
2021-06-16 13:19:45 +0100

    deployed version of MailChimp App

--- Web Development/Mailchimp/app.js
+++ Web Development/Mailchimp/app.js
@@ -73,6 +73,6 @@ app.post("/failure", function(req, res){
 })
 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Server is running on port 3000");
 })
