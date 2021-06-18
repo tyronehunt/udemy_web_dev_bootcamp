5302dba8460b6eba0e1a59bf5b33a716eff0c9a3
Tyrone Hunt <tyrone.hunt07@gmail.com>
2021-06-18 17:37:11 +0100

    added css styling to todolist

--- Web Development/toDoList EJS/app.js
+++ Web Development/toDoList EJS/app.js
@@ -9,6 +9,7 @@ var items = ["Add an item like this"];
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res) {
 
