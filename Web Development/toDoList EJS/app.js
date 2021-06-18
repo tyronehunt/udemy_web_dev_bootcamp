a3aece275219f28b2c3b8ad8ba9f97083d95cfe6
Tyrone Hunt <tyrone.hunt07@gmail.com>
2021-06-18 16:53:29 +0100

    working add list item functionality via EJS

--- Web Development/toDoList EJS/app.js
+++ Web Development/toDoList EJS/app.js
@@ -3,45 +3,42 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+// Set placeholder for to do list item
+var items = ["Add an item like this"];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
 
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log("Error: current day is equal to " + currentDay);
-  }
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  var day = today.toLocaleDateString("en-US", options);
+
 
   res.render('list', {
-    kindOfDay: day
+    kindOfDay: day,
+    newListItems: items
   });
 
 });
+
+// Post from form in list.ejs
+app.post("/", function(req, res){
+  item = req.body.newItem;
+
+  items.push(item);
+
+  res.redirect("/")
+
+})
 
 
 app.listen(3000, function() {
