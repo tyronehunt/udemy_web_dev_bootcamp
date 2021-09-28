9e07728890a3c55d503f4a966f35dc5e99c8bd43
Tyrone Hunt <tyrone.hunt@king.com>
2021-09-28 15:26:16 +0100

    added localStorage capability

--- react_usecontext/src/contexts/BookContext.js
+++ react_usecontext/src/contexts/BookContext.js
@@ -1,11 +1,19 @@
 
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer, useEffect } from 'react';
 import { bookReducer } from '../reducers/bookReducer';
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-  const [books, dispatch] = useReducer(bookReducer, []);
+  const [books, dispatch] = useReducer(bookReducer, [], () => {
+      const localData = localStorage.getItem('books');
+      return localData ? JSON.parse(localData) : []
+  });
+
+    // Whenever books data object changes run this hook
+    useEffect(() => {
+        localStorage.setItem('books', JSON.stringify(books))
+    }, [books]);
 
   return (
     <BookContext.Provider value={{ books, dispatch}}>
